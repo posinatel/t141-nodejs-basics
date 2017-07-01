3a5b35af72abeb8834cfee7e60e2d1d969feb5e9
Edy Segura <ednardo@inatel.br>
2017-07-01 02:36:55 -0300

    E2E integration

--- 06-express/public/js/app.js
+++ 06-express/public/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', []);
 
-app.service('SubscriberService', function () {
+app.service('SubscriberService', function ($http) {
     //to create unique subscriber id
     var uid = 1;
 
@@ -17,59 +17,74 @@ app.service('SubscriberService', function () {
         if (subscriber.id == null) {
             //if this is new subscriber, add it in subscribers array
             subscriber.id = uid++;
-            subscribers.push(subscriber);
+            return $http.post('/api/subscribers', subscriber)
+            // subscribers.push(subscriber);
         } else {
             //for existing subscriber, find this subscriber using id
             //and update it.
-            for (i in subscribers) {
-                if (subscribers[i].id == subscriber.id) {
-                    subscribers[i] = subscriber;
-                }
-            }
+            return $http.put('/api/subscribers/' + subscriber.id, subscriber)
+            // for (i in subscribers) {
+            //     if (subscribers[i].id == subscriber.id) {
+            //         subscribers[i] = subscriber;
+            //     }
+            // }
         }
     }
 
     //simply search subscribers list for given id
     //and returns the subscriber object if found
     this.get = function (id) {
-        for (i in subscribers) {
-            if (subscribers[i].id == id) {
-                return subscribers[i];
-            }
-        }
-
+        return $http.get('/api/subscribers/' + id)
+        // for (i in subscribers) {
+        //     if (subscribers[i].id == id) {
+        //         return subscribers[i];
+        //     }
+        // }
     }
 
     //iterate through subscribers list and delete
     //subscriber if found
     this.delete = function (id) {
-        for (i in subscribers) {
-            if (subscribers[i].id == id) {
-                subscribers.splice(i, 1);
-            }
-        }
+        return $http.delete('/api/subscribers/' + id)
+        // for (i in subscribers) {
+        //     if (subscribers[i].id == id) {
+        //         subscribers.splice(i, 1);
+        //     }
+        // }
     }
 
     //simply returns the subscribers list
     this.list = function () {
-        return subscribers;
+        return $http.get('/api/subscribers')
     }
 });
 
 app.controller('SubscriberController', function ($scope, SubscriberService) {
-    $scope.subscribers = SubscriberService.list();
+    function list() {
+        SubscriberService.list().then(response => {
+            console.log(response.data)
+            $scope.subscribers = response.data;
+        });
+    }
+    list();
 
     $scope.saveSubscriber = function () {
-        SubscriberService.save($scope.newSubscriber);
-        $scope.newSubscriber = {};
+        SubscriberService.save($scope.newSubscriber).then(response => {
+            $scope.newSubscriber = {};
+            list();
+        });
     }
 
     $scope.delete = function (id) {
-        SubscriberService.delete(id);
-        if ($scope.newSubscriber.id == id) $scope.newSubscriber = {};
+        SubscriberService.delete(id).then(response => {
+            $scope.newSubscriber = {};
+            list();
+        });
     }
 
     $scope.edit = function (id) {
-        $scope.newSubscriber = angular.copy(SubscriberService.get(id));
+        SubscriberService.get(id).then(response => {
+            $scope.newSubscriber = angular.copy(response.data);
+        })
     }
 });
